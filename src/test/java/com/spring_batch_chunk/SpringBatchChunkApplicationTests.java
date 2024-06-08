8bd19eae93996853ed8ba3c7dcbb5ef5e4ffeea4
ednaleiden <ednadupont@hotmail.com>
2024-06-08 18:56:09 -0500

    spring batch chunk

---
+++ src/test/java/com/spring_batch_chunk/SpringBatchChunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_batch_chunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchChunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
