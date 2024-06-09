68b5d0b59cd25d470c2b2bdfbed936c677f32e47
ednaleiden <ednadupont@hotmail.com>
2024-06-09 09:45:11 -0500

    modify of connexion and config of spring batch

---
+++ src/main/java/com/spring_batch_chunk/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.spring_batch_chunk.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/batch_chunk");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("");
+
+        return driverManagerDataSource;
+        }
+}
+
